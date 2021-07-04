2407e97292b4a78a0f172e2566ed4ba4406ed3b5
Dunghedspi <dung.nv.soict@gmail.com>
2021-07-04 15:30:03 +0700

    add_ssl

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RedisIoAdapter } from './adapters/redis.adapter';
 import * as cookieParser from 'cookie-parser';
-
+import fs from "fs"
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/private-key.pem'),
+    cert: fs.readFileSync('./secrets/public-certificate.pem'),
+  };
+  const app = await NestFactory.create(AppModule, {httpsOptions});
   app.useWebSocketAdapter(new RedisIoAdapter(app));
   const PORT = Number.parseInt(process.env.PORT);
   app.enableCors({ credentials: true, origin: process.env.CLIENT_DOMAIN });
